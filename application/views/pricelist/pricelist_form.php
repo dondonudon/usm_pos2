30fb682169809d6b5f86a3efa3bec8ffa76c2275
dondonudon <alvianto.prasetyo@live.com>
2020-07-02 23:03:28 +0700

    update button 'kembali'

--- application/views/pricelist/pricelist_form.php
+++ application/views/pricelist/pricelist_form.php
@@ -16,15 +16,15 @@
 		<?php
 $query = $this->db->query("SELECT * FROM range_pricelist ORDER BY id ASC")->result_array();
 foreach ($query as $key) {
- echo "<input type=\"hidden\" name=\"range_pricelist[]\" id=\"range_pricelist[]\" value=" . $key['id'] . ">";
- echo "<tr><td width='200'>" . $key['nama'] . "</td><td><input type=\"text\" class=\"form-control\" name=\"harga[]\" id=\"harga[]\" placeholder=\"Harga\"></td></tr>";
- //  echo $key['kode_kasir'];
- //  echo $key['nama'];
+    echo "<input type=\"hidden\" name=\"range_pricelist[]\" id=\"range_pricelist[]\" value=" . $key['id'] . ">";
+    echo "<tr><td width='200'>" . $key['nama'] . "</td><td><input type=\"text\" class=\"form-control\" name=\"harga[]\" id=\"harga[]\" placeholder=\"Harga\"></td></tr>";
+    //  echo $key['kode_kasir'];
+    //  echo $key['nama'];
 }
 ?>
 	    <tr><td></td><td><input type="hidden" name="id" value="<?php echo $id; ?>" />
 	    <button type="submit" class="btn btn-danger"><i class="fa fa-floppy-o"></i> <?php echo $button ?></button>
-	    <a href="<?php echo site_url('tab_pricelist') ?>" class="btn btn-info"><i class="fa fa-sign-out"></i> Kembali</a></td></tr>
+	    <a href="<?php echo site_url('pricelist') ?>" class="btn btn-info"><i class="fa fa-sign-out"></i> Kembali</a></td></tr>
 	</table></form>        </div>
 </div>
 </div>
--- application/views/range_pricelist/range_pricelist_form.php
+++ application/views/range_pricelist/range_pricelist_form.php
@@ -15,7 +15,7 @@
 	    <tr><td width='200'>QTY Akhir <?php echo form_error('qty_b') ?></td><td><input type="text" class="form-control" name="qty_b" id="qty_b" placeholder="QTY AKHIR" value="<?php echo $qty_b; ?>" /></td></tr>
 	    <tr><td></td><td><input type="hidden" name="id" value="<?php echo $id; ?>" />
 	    <button type="submit" class="btn btn-danger"><i class="fa fa-floppy-o"></i> <?php echo $button ?></button>
-	    <a href="<?php echo site_url('tab_kasir') ?>" class="btn btn-info"><i class="fa fa-sign-out"></i> Kembali</a></td></tr>
+	    <a href="<?php echo site_url('range_pricelist') ?>" class="btn btn-info"><i class="fa fa-sign-out"></i> Kembali</a></td></tr>
 	</table></form>        </div>
 </div>
 </div>
