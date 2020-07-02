45eff7f50e9d3877e61646884c795eda7dd550ff
dondonudon <alvianto.prasetyo@live.com>
2020-07-02 23:49:22 +0700

    join mst_kategori

--- application/models/Mst_barang_model.php
+++ application/models/Mst_barang_model.php
@@ -1,7 +1,8 @@
 <?php
 
-if (!defined('BASEPATH'))
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
+}
 
 class Mst_barang_model extends CI_Model
 {
@@ -10,83 +11,86 @@ class Mst_barang_model extends CI_Model
     public $id = 'id';
     public $order = 'DESC';
 
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
     }
 
     // datatables
-    function json() {
-        $this->datatables->select('barang,datetime,harga,id,id_kategori,stok,ukuran,use_pricelist,use_stok');
+    public function json()
+    {
+        $this->datatables->select('mst_barang.barang,mst_barang.datetime,mst_barang.harga,mst_barang.id,mst_barang.id_kategori,mst_barang.stok,mst_barang.ukuran,mst_barang.use_pricelist,mst_barang.use_stok,mst_kategori.kategori');
         $this->datatables->from('mst_barang');
         //add this line for join
-        //$this->datatables->join('table2', 'mst_barang.field = table2.field');
-        $this->datatables->add_column('action', anchor(site_url('mst_barang/read/$1'),'<i class="fa fa-eye" aria-hidden="true"></i>', array('class' => 'btn btn-danger btn-sm'))." 
-            ".anchor(site_url('mst_barang/update/$1'),'<i class="fa fa-pencil-square-o" aria-hidden="true"></i>', array('class' => 'btn btn-danger btn-sm'))." 
-                ".anchor(site_url('mst_barang/delete/$1'),'<i class="fa fa-trash-o" aria-hidden="true"></i>','class="btn btn-danger btn-sm" onclick="javasciprt: return confirm(\'Are You Sure ?\')"'), 'id');
+        $this->datatables->join('mst_kategori', 'mst_barang.id_kategori = mst_kategori.id');
+        $this->datatables->add_column('action', anchor(site_url('mst_barang/read/$1'), '<i class="fa fa-eye" aria-hidden="true"></i>', array('class' => 'btn btn-danger btn-sm')) . "
+            " . anchor(site_url('mst_barang/update/$1'), '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>', array('class' => 'btn btn-danger btn-sm')) . "
+                " . anchor(site_url('mst_barang/delete/$1'), '<i class="fa fa-trash-o" aria-hidden="true"></i>', 'class="btn btn-danger btn-sm" onclick="javasciprt: return confirm(\'Are You Sure ?\')"'), 'id');
         return $this->datatables->generate();
     }
 
     // get all
-    function get_all()
+    public function get_all()
     {
         $this->db->order_by($this->id, $this->order);
         return $this->db->get($this->table)->result();
     }
 
     // get data by id
-    function get_by_id($id)
+    public function get_by_id($id)
     {
         $this->db->where($this->id, $id);
         return $this->db->get($this->table)->row();
     }
-    
+
     // get total rows
-    function total_rows($q = NULL) {
+    public function total_rows($q = null)
+    {
         $this->db->like('id', $q);
-	$this->db->or_like('barang', $q);
-	$this->db->or_like('datetime', $q);
-	$this->db->or_like('harga', $q);
-	$this->db->or_like('id_kategori', $q);
-	$this->db->or_like('stok', $q);
-	$this->db->or_like('ukuran', $q);
-	$this->db->or_like('use_pricelist', $q);
-	$this->db->or_like('use_stok', $q);
-	$this->db->from($this->table);
+        $this->db->or_like('barang', $q);
+        $this->db->or_like('datetime', $q);
+        $this->db->or_like('harga', $q);
+        $this->db->or_like('id_kategori', $q);
+        $this->db->or_like('stok', $q);
+        $this->db->or_like('ukuran', $q);
+        $this->db->or_like('use_pricelist', $q);
+        $this->db->or_like('use_stok', $q);
+        $this->db->from($this->table);
         return $this->db->count_all_results();
     }
 
     // get data with limit and search
-    function get_limit_data($limit, $start = 0, $q = NULL) {
+    public function get_limit_data($limit, $start = 0, $q = null)
+    {
         $this->db->order_by($this->id, $this->order);
         $this->db->like('id', $q);
-	$this->db->or_like('barang', $q);
-	$this->db->or_like('datetime', $q);
-	$this->db->or_like('harga', $q);
-	$this->db->or_like('id_kategori', $q);
-	$this->db->or_like('stok', $q);
-	$this->db->or_like('ukuran', $q);
-	$this->db->or_like('use_pricelist', $q);
-	$this->db->or_like('use_stok', $q);
-	$this->db->limit($limit, $start);
+        $this->db->or_like('barang', $q);
+        $this->db->or_like('datetime', $q);
+        $this->db->or_like('harga', $q);
+        $this->db->or_like('id_kategori', $q);
+        $this->db->or_like('stok', $q);
+        $this->db->or_like('ukuran', $q);
+        $this->db->or_like('use_pricelist', $q);
+        $this->db->or_like('use_stok', $q);
+        $this->db->limit($limit, $start);
         return $this->db->get($this->table)->result();
     }
 
     // insert data
-    function insert($data)
+    public function insert($data)
     {
         $this->db->insert($this->table, $data);
     }
 
     // update data
-    function update($id, $data)
+    public function update($id, $data)
     {
         $this->db->where($this->id, $id);
         $this->db->update($this->table, $data);
     }
 
     // delete data
-    function delete($id)
+    public function delete($id)
     {
         $this->db->where($this->id, $id);
         $this->db->delete($this->table);
